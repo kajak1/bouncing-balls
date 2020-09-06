9ba57c62ca34146e0186104d7d7878d10606a00d
kajak1 <kajetan.korzonkiewicz@gmail.com>
2020-09-06 23:17:29 +0200

    added gravity

--- js/Ball.js
+++ js/Ball.js
@@ -2,10 +2,24 @@ import { canvas, ctx, random } from './constants.js';
 
 class Ball {
   constructor() {
-    this.radius = 20;
+    this.radius = 5;
     this.x = random(0, canvas.width - 2 * this.radius);
     this.y = random(-50, 600);
-    this.speed = random(10, 20);
+    this.speed = random(5, 11);
+    this.momentum = 0;
+  }
+
+  gravity() {
+    if (this.x != canvas.height) {
+      this.speed += 0.3;
+    }
+  }
+
+  collisionDetection() {
+    if (this.y >= canvas.height) {
+      this.y = canvas.height;
+      this.speed *= -1 / 2;
+    }
   }
 
   draw() {
@@ -13,7 +27,7 @@ class Ball {
     ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
     ctx.fillStyle = 'white';
     ctx.fill();
-    ctx.stroke();
+    // ctx.stroke();
   }
 
   move() {
@@ -22,6 +36,8 @@ class Ball {
 
   fall() {
     this.draw();
+    this.collisionDetection();
+    this.gravity();
     this.move();
   }
 }
